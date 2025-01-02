0e3885a23feaa6a795659dc7daefc9637030fd4a
syukinmei <347086009@qq.com>
2025-01-02 17:33:29 +0800

    feat(utils):add log method

--- packages/utils/lib/index.js
+++ packages/utils/lib/index.js
@@ -1,6 +1,8 @@
 import { makeInquirer } from "./inquirer.js";
 import { getLatestVersion } from "./npm.js";
 import httpRequest from "./httpRequest.js";
+import log from "./log.js";
+
 import * as nodeEnhance from "./nodeEnhance.js";
 
-export { makeInquirer, getLatestVersion, httpRequest, nodeEnhance };
+export { makeInquirer, getLatestVersion, httpRequest, log, nodeEnhance };
---
+++ packages/utils/lib/log.js
@@ -0,0 +1,31 @@
+// example：
+// log.success("成功了", "some message...");
+// log.warn("注意", "some message...");
+// log.error("出错了", "some message...");
+// log.result("some message...");
+import log from "npmlog";
+
+log.heading = process.argv[2];
+
+log.headingStyle = {
+    fg: "brightYellow",
+};
+
+if (process.argv.includes("--debug", "-d")) {
+    log.level = "verbose";
+} else {
+    log.level = "success";
+}
+
+log.addLevel("success", 3500, { fg: "green", bold: true });
+
+log.result = (info) =>
+    console.log(
+        `\x1b[32m
+    +-----------------------------+
+    |          RESULT!            |
+    +-----------------------------+
+        \x1b[0m` + `${info}`
+    );
+
+export default log;
